e6d66e1db2e8bcbf8440a49b8ac45d5c64cd5fdb
topg85 <danielcarson1985@gmail.com>
2025-09-26 18:51:15 +0100

    Fix ALLOWED_HOSTS and static files for Heroku deployment

--- shutterspace/settings.py
+++ shutterspace/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['django-project-shutterspace.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'django-project-shutterspace.herokuapp.com', 
+    'django-project-shutterspace-a676bf7fbd5b.herokuapp.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
@@ -134,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
