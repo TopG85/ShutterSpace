ea539b029c932da37dee48dd2453d540483c3c5d
topg85 <danielcarson1985@gmail.com>
2025-09-30 10:17:52 +0100

    deployment settings

--- shutterspace/settings.py
+++ shutterspace/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Additional locations of static files for development
@@ -149,6 +150,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',  # project-level static directory where static/css/style.css lives
     BASE_DIR / 'shutterspace' / 'static',
 ]
+
+# Use WhiteNoise static files storage so collectstatic produces hashed filenames and
+# WhiteNoise can serve compressed static files in production (Heroku).
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
